484dfebf146f8024a57d3f630b14c383858c2a78
gndpwnd <gnelsondev@gmail.com>
2022-10-31 13:04:37 -0800

    update

--- lady_in_the_cage/src/main.cpp
+++ lady_in_the_cage/src/main.cpp
@@ -3,9 +3,6 @@
 
 struct {
 
-    int us1e = 5;
-    int us1t = 6;
-
     int pn_1 = 9; // Right
     int pn_2 = 10; // Left
 
@@ -15,35 +12,46 @@ struct {
 
 struct {
 
-    int BAUD = 9600;
+    int max_timer = 100000;
+    int min_timer = 10000;
 
-    int min_dist = 45;
-    int measure_delay = 250; // miliseconds
+    int BAUD = 9600;
 
 
     int status_blinks = 3; // blink led to notify of activation
     int status_delay = 250; // miliseconds
 
-    int pneumatic_cylinder_travel_time = 3000; // miliseconds
+    int pneumatic_cylinder_travel_time = 75; // miliseconds
 
-    int max_delay_between_right_left = 100; // deciseconds 
+    int max_delay_between_right_left = 100; 
     int min_delay_between_right_left = 25;
 
-    int max_delay_between_shakes = 100; // deciseconds
-    int min_delay_between_shakes = 50;
+    int max_delay_between_shakes = 50;
+    int min_delay_between_shakes = 5;
+
+    int max_num_shakes = 100;
+    int min_num_shakes = 25;
 
 } info;
 
-Ultrasonic us1(pins.us1t, pins.us1e);
-
 float rand_delay_between_right_left() {
     int rand_int = random(info.min_delay_between_right_left, info.max_delay_between_right_left);
-    return rand_int * 10.0;
+    return rand_int;
 }
 
 float rand_delay_between_shakes() {
     int rand_int = random(info.min_delay_between_shakes, info.max_delay_between_shakes);
-    return rand_int * 10.0;
+    return rand_int;
+}
+
+float rand_num_shakes() {
+    int rand_int = random(info.min_num_shakes, info.max_num_shakes);
+    return rand_int;
+}
+
+float rand_timer() {
+    int rand_int = random(info.min_timer, info.max_timer);
+    return rand_int;
 }
 
 void right_high() {
@@ -100,29 +108,17 @@ void setup() {
 
 void loop() {
 
-    float dist = us1.Ranging(CM);
-    //Serial.print("Distance: ");
-    //Serial.println(dist);
-
-    if (dist <= info.min_dist) {
         
-        status_blink();
-        
-        while (dist <= info.min_dist) {
-            right_high();
-            delay(rand_delay_between_shakes());
-            left_high();
-            delay(rand_delay_between_shakes());
-            dist = us1.Ranging(CM);
-            
-            if (dist > info.min_dist) {
-                off();
-                break;
-            }
+    status_blink();
 
-        }
-
+    for (int i=0; i<=rand_num_shakes(); i++) {
+        right_high();
+        delay(rand_delay_between_shakes());
+        left_high();
+        delay(rand_delay_between_shakes());
     }
+    off();
 
-    delay(info.measure_delay);
+
+    delay(rand_timer());
 };
